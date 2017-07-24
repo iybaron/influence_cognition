642e5fcc61678fbaabb25391568f45d191f72774
Isaac <iybaron@gmail.com>
2017-07-23 18:17:30 -0700

    Added comments to unify_grade_dtypes.py

--- unify_grade_dtype.py
+++ unify_grade_dtype.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 k12_data = pd.read_csv('data/data_k12_orig_wgrade.csv')
 
+# Change all char values of 'GRADE' to ints
 k12_data.loc[k12_data['GRADE'] == 'K', 'GRADE'] = 0
 k12_data.loc[k12_data['GRADE'] == 'P', 'GRADE'] = 13
 k12_data.loc[k12_data['GRADE'] == 'S', 'GRADE'] = 14
 k12_data.loc[k12_data['GRADE'] == 'T', 'GRADE'] = 15
 k12_data.loc[k12_data['GRADE'] == 'U', 'GRADE'] = 16
 
+# Change all char values of 'GRADEEQ' to ints
 k12_data.loc[k12_data['GRADEEQ'] == 'K', 'GRADEEQ'] = 0
 k12_data.loc[k12_data['GRADEEQ'] == 'T', 'GRADEEQ'] = 15
 k12_data.loc[k12_data['GRADEEQ'] == 'U', 'GRADEEQ'] = 16
